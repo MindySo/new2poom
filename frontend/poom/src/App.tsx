f1a7a3228467ee43772b2c8c2d65dcc452e66701
wang seong min <163216266+wang-arthur-dev@users.noreply.github.com>
2025-11-05 01:31:05 +0900

    feat(S13P31A706-70): 공통 탑바 레이아웃 조정

--- frontend/poom/src/App.tsx
+++ frontend/poom/src/App.tsx
@@ -6,6 +6,7 @@ import DevPage from './pages/DevPage/DevPage';
 import MapPage from './pages/MapPage';
 import ListPage from './pages/ListPage/ListPage';
 import ReportPage from './pages/ReportPage/ReportPage';
+import styles from './App.module.css';
 
 function App() {
   const isMobile = useIsMobile(1024);
@@ -13,12 +14,14 @@ function App() {
   return (
     <Router>
       {isMobile ? <MobileTopBar /> : <TopBar />}
-      <Routes>
-        <Route path="/" element={<DevPage />} />
-        <Route path="/map" element={<MapPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/report" element={<ReportPage />} />
-      </Routes>
+      <div className={`${styles.appContainer} ${isMobile ? styles.mobile : ''}`}>
+        <Routes>
+          <Route path="/" element={<DevPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/report" element={<ReportPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
